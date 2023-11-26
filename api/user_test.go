a5f046d868b132cfb73d5e2d171b8861ff9a0cb6
he-mac <1249844504@qq.com>
2023-11-26 13:08:57 +0800

    测试创建用户api

---
+++ api/user_test.go
@@ -0,0 +1,158 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	mockdb "simplebank/db/mock"
+	db "simplebank/db/sqlc"
+	"simplebank/utils"
+	"testing"
+)
+
+type eqCreateUserParamsMatcher struct {
+	arg      db.CreateUserParams
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x any) bool {
+	// 校验数据类型
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	// 对密码进行校验
+	err := utils.CheckPassowrd(e.password, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+	e.arg.HashedPassword = arg.HashedPassword
+
+	return reflect.DeepEqual(e.arg, arg)
+}
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
+
+func TestCreateUserAPI(t *testing.T) {
+	// 创建随机用户
+	user, password := randomUser(t)
+	// 添加测试用例
+	// 1. 包含用例的名字
+	// 2. 参数...
+	// 3. mock数据库校验
+	// 4. 响应校验
+	var testCasse = []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"username":  user.Username,
+				"password":  password,
+				"full_name": user.FullName,
+				"email":     user.Email,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+
+				arg := db.CreateUserParams{
+					Username:       user.Username,
+					HashedPassword: password,
+					FullName:       user.FullName,
+					Email:          user.Email,
+				}
+				store.EXPECT().CreateUser(gomock.Any(), EqCreateUserParams(arg, password)).
+					Times(1).Return(user, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchUser(t, recorder.Body, user)
+			},
+		},
+	}
+
+	// 对测试用例中的例子进行测试
+	for _, tc := range testCasse {
+		// 开启子测试
+		t.Run(tc.name, func(t *testing.T) {
+			// 创建一个gomock控制器
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			// 创建gomock数据库
+			store := mockdb.NewMockStore(ctrl)
+
+			// 构建gomock预期调用的数据
+			tc.buildStubs(store)
+
+			// 开启gin服务
+			server := NewServer(store)
+
+			url := fmt.Sprintf("/users")
+			// 构造Json数据
+			jsonData, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+			require.NoError(t, err)
+
+			// 生成一个响应记录器
+			recorder := httptest.NewRecorder()
+			// 发送请求，并将结果存储到记录器中
+			server.router.ServeHTTP(recorder, request)
+
+			// 检查响应
+			tc.checkResponse(t, recorder)
+		})
+	}
+
+}
+
+// 创建随机用户
+func randomUser(t *testing.T) (user db.User, password string) {
+	// 生成一个随机密码
+	password = utils.RandomString(6)
+	hashPassword, err := utils.HashPassword(password)
+	require.NoError(t, err)
+
+	// 构建用户
+	user = db.User{
+		Username:       utils.RandomOwner(),
+		HashedPassword: hashPassword,
+		FullName:       utils.RandomOwner(),
+		Email:          utils.RandomEamil(),
+	}
+
+	return
+}
+
+// 验证用户Body数据
+func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
+	// 将Buffer数据读取出来
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotUser db.User
+	err = json.Unmarshal(data, &gotUser)
+	require.NoError(t, err)
+
+	require.Equal(t, user.Username, gotUser.Username)
+	require.Equal(t, user.Email, gotUser.Email)
+	require.Equal(t, user.FullName, gotUser.FullName)
+	require.Empty(t, gotUser.HashedPassword)
+}
